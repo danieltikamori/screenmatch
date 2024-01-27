de1c82bc70d5d0c63dd4cbe52b607dbaa7a91b91
Daniel Tikamori <tikamori.itiro@gmail.com>
2024-01-27 21:39:18 +0900

    Add Movie class

---
+++ src/Movie.java
@@ -0,0 +1,13 @@
+public class Movie {
+    String movieTitle;
+    int launchDate;
+    boolean includedInPlan;
+    double movieGrade;
+    int totalReviewCount;
+    int totalRatingCount;
+    double averageRating;
+    int movieDuration;
+    String movieGenre;
+    String movieDirector;
+}
+
