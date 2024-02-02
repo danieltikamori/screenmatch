f372a2f13480bc8712a7f5a439011f5a16dec70e
Daniel Tikamori <tikamori.itiro@gmail.com>
2024-02-02 20:09:35 +0900

    Create first methods in Java

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,20 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Movie myMovie = new Movie();
+        myMovie.movieTitle = "Star Wars";
+        myMovie.launchDate = 1977;
+        myMovie.includedInPlan = true;
+        myMovie.movieDuration = 200;
+        myMovie.movieGenre = "Sci-Fi";
+
+        myMovie.showMovieDetails();
+        myMovie.calculateMovieGrade(9.5);
+        myMovie.calculateMovieGrade(8.5);
+        myMovie.calculateMovieGrade(7.5);
+        System.out.println(myMovie.movieGradeSum);
+        System.out.println(myMovie.totalGradeCount);
+        System.out.println(myMovie.calculateAverageMovieGrade());
 
     }
 }
--- src/Movie.java
+++ src/Movie.java
@@ -3,11 +3,36 @@ public class Movie {
     int launchDate;
     boolean includedInPlan;
     double movieGrade;
+    double movieGradeSum;
+    int totalGradeCount;
+    double averageMovieGrade;
     int totalReviewCount;
-    int totalRatingCount;
-    double averageRating;
+    double averageReview;
     int movieDuration;
     String movieGenre;
     String movieDirector;
+
+    void showMovieDetails() {
+        System.out.println("Movie Title: " + movieTitle);
+        System.out.println("Launch Date: " + launchDate);
+        System.out.println("Included in plan: " + includedInPlan);
+        System.out.println("Movie Grade: " + movieGrade);
+        System.out.println("Total Review Count: " + totalGradeCount);
+        System.out.println("Average Movie Grade: " + averageMovieGrade);
+        System.out.println("Total Rating Count: " + totalReviewCount);
+        System.out.println("Average Rating: " + averageReview);
+        System.out.println("Movie Duration: " + movieDuration);
+        System.out.println("Movie Genre: " + movieGenre);
+        System.out.println("Movie Director: " + movieDirector);
+    }
+
+    void calculateMovieGrade(double grade) {
+        movieGradeSum += grade;
+        totalGradeCount++;
+    }
+
+    double calculateAverageMovieGrade() {
+        return movieGradeSum / totalGradeCount;
+    }
 }
 
